6b51d69e61fc02ef3fce5d3e34b1908ca2984739
Jules Debeaumont <deb.jules@outlook.fr>
2022-02-21 21:35:14 +0100

    Front: sidebar component wip

---
+++ Front/src/components/Sidebar.js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+
+function Sidebar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      {/* Sidebar */}
+      <div className="hidden sm:flex">
+        <div className="top-0 left-0 w-[20vw] bg-indigo-900 p-10 fixed h-full z-5">
+
+          <div className="flex text-center align-middle justify-center">
+            <h3 className="my-8 text-4xl font-semibold text-slate-200">I am a sidebar</h3>
+          </div>
+
+          <div className="flex text-center align-middle justify-center">
+            <h3 className="my-8 text-4xl font-semibold text-slate-200">I am a sidebar</h3>
+          </div>
+
+          <div className="flex text-center align-middle justify-center">
+            <h3 className="my-8 text-4xl font-semibold text-slate-200">I am a sidebar</h3>
+          </div>
+
+          <div className="flex text-center align-middle justify-center">
+            <h3 className="my-8 text-4xl font-semibold text-slate-200">I am a sidebar</h3>
+          </div>
+
+        </div>
+      </div>
+
+
+      {/* Menu for small screen */}
+      <div className="flex sm:hidden">
+        {isOpen ? (
+          <>
+            <div className={`top-0 left-0 bg-indigo-900 p-5 fixed w-full z-5 transition ease-in-out duration-300 ${isOpen ? "translate-y-0 " : "translate-y-full"}`}>
+
+              <div className="flex justify-end">
+                <div onClick={() => setIsOpen(!isOpen)}>close me</div>
+              </div>
+
+              <div className="flex text-center align-middle justify-center">
+                <h3 className="my-8 text-4xl font-semibold text-slate-200">I am a sidebar</h3>
+              </div>
+
+              <div className="flex text-center align-middle justify-center">
+                <h3 className="my-8 text-4xl font-semibold text-slate-200">I am a sidebar</h3>
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="fixed top-0 left-0 p-3 flex justify-start">
+            <div onClick={() => setIsOpen(!isOpen)}>open me</div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default Sidebar;
